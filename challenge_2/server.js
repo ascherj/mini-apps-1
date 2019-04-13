ea2c1d7ce7bf0aff69add5d90eae986a247f3714
Jake Ascher <ascherj@gmail.com>
2019-04-13 16:16:01 -0400

    POST form data to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use(express.static('client'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/', (req, res) => {
+  const text = req.body.csv;
+  res.redirect('/');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
